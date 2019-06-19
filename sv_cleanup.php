39a3657425b450f84a72247fe68bef94978e04ef
Matthias Bathke <matthias-bathke@straightvisions.com>
2019-06-19 14:48:11 +0200

    REFACTORING, SETTINGS for Features

--- sv_cleanup.php
+++ sv_cleanup.php
@@ -12,23 +12,50 @@ namespace sv_100;
  */
 
 class sv_cleanup extends init {
-	public function __construct() {
-
-	}
-
 	public function init() {
 		// Module Info
 		$this->set_module_title( 'SV Cleanup' );
-		$this->set_module_desc( __( 'This module removes default stylings from the browser and plugins.', $this->get_module_name() ) );
+		$this->set_module_desc( __( 'Improve some WordPress Standards', $this->get_module_name() ) );
+		
+		// Section Info
+		$this->set_section_title( __( 'Cleanup', 'straightvisions_100' ) )
+			 ->set_section_desc( __( 'Improve some WordPress Standards', 'straightvisions_100' ) )
+			 ->set_section_type( 'settings' );
+		
+		$this->get_root()->add_section($this);
+		
+		$this->load_settings();
+		
+		if($this->s['jquery_migrate']->run_type()->get_data()){
+			add_action( 'wp_default_scripts', array($this, 'jquery_migrate') );
+		}
+		if($this->s['meta_data']->run_type()->get_data()){
+			$this->meta_data();
+		}
+		if($this->s['emoji_styles']->run_type()->get_data()){
+			remove_action('wp_print_styles', 'print_emoji_styles'); // remove emoji
+		}
+		if($this->s['wp_media']->run_type()->get_data()){
+			add_action('wp_print_styles', array($this, 'wp_print_styles'), 100);
+			add_action('wp_print_footer_scripts', array($this, 'wp_print_styles'), 1);
+		}
 
 		// Action Hooks
 		add_action('wp_head', array($this, 'wp_start'), 1);
 		add_action('wp_footer', array($this, 'wp_end'), 9999999);
-		// WP media
-		add_action('wp_print_styles', array($this, 'wp_print_styles'), 100);
-		add_action('wp_print_footer_scripts', array($this, 'wp_print_styles'), 1);
-		remove_action('wp_print_styles', 'gforms_css'); // remove gravity form styles
-		remove_action('wp_print_styles', 'print_emoji_styles'); // remove emoji
+		
+		// lazy load attached CSS
+		add_filter('rocket_buffer', function($buffer){ return str_replace('rel="stylesheet"', 'rel="stylesheet" media="none" onload="if(media!=\'all\')media=\'all\'"', $buffer); }, 999999);
+	}
+	public function jquery_migrate($scripts){
+		if ( ! is_admin() && ! empty( $scripts->registered['jquery'] ) ) {
+			$scripts->registered['jquery']->deps = array_diff(
+				$scripts->registered['jquery']->deps,
+				[ 'jquery-migrate' ]
+			);
+		}
+	}
+	public function meta_data(){
 		remove_action('wp_head', 'rest_output_link_wp_head', 10); // Remove api.w.org REST API from WordPress header
 		remove_action('wp_head', 'wp_oembed_add_discovery_links', 10); // Remove api.w.org REST API from WordPress header
 		remove_action('wp_head', 'rsd_link');
@@ -36,19 +63,76 @@ class sv_cleanup extends init {
 		remove_action('wp_head', 'wp_shortlink_wp_head');
 		remove_action('wp_head', 'wp_generator');
 	}
-
+	public function load_settings(): sv_cleanup {
+		$this->s['jquery_migrate'] =
+			$this->get_setting()
+				 ->set_ID( 'jquery_migrate' )
+				 ->set_title( __( 'Disable jQuery Migrate', 'straightvisions_100' ) )
+				 ->set_description( __( 'In most cases, you will not need jQuery-Migrate. Disabling this script will reduce pageload and improve Pagespeed. Check for Javascript-Errors in Frontend after activating this.', 'straightvisions_100' ) )
+				 ->load_type( 'checkbox' );
+		
+		$this->s['meta_data'] =
+			$this->get_setting()
+				 ->set_ID( 'meta_data' )
+				 ->set_title( __( 'Remove non-critical meta data', 'straightvisions_100' ) )
+				 ->set_description( __( 'Removes some lines of HTML-Meta-Data which are not critical for your site, but saves some byte of code in the frontend: rest_output_link_wp_head, wp_oembed_add_discovery_links, rsd_link, wlwmanifest_link, wp_shortlink_wp_head, wp_generator', 'straightvisions_100' ) )
+				 ->load_type( 'checkbox' );
+		
+		$this->s['emoji_styles'] =
+			$this->get_setting()
+				 ->set_ID( 'emoji_styles' )
+				 ->set_title( __( 'Removes Emoji Styles', 'straightvisions_100' ) )
+				 ->set_description( __( 'Instead of loading those styles from WP, default browser emojis will be displayed', 'straightvisions_100' ) )
+				 ->load_type( 'checkbox' );
+		
+		$this->s['wp_media'] =
+			$this->get_setting()
+				 ->set_ID( 'wp_media' )
+				 ->set_title( __( 'Load WP Media Styles inline', 'straightvisions_100' ) )
+				 ->set_description( __( 'To optimize your Pagespeed Score, you may need to load WP Media Styles inline if loaded. Activate this, if Pagespeed Test Tool says external WP Media Styles are renderblocking.', 'straightvisions_100' ) )
+				 ->load_type( 'checkbox' );
+		
+		$this->s['wp_media'] =
+			$this->get_setting()
+				 ->set_ID( 'wp_media' )
+				 ->set_title( __( 'Load WP Media Styles inline', 'straightvisions_100' ) )
+				 ->set_description( __( 'To optimize your Pagespeed Score, you may need to load WP Media Styles inline if loaded. Activate this, if Pagespeed Test Tool says external WP Media Styles are renderblocking.', 'straightvisions_100' ) )
+				 ->load_type( 'checkbox' );
+		
+		$this->s['alt_attr'] =
+			$this->get_setting()
+				 ->set_ID( 'alt_attr' )
+				 ->set_title( __( 'Add alt-attributes to images if missing', 'straightvisions_100' ) )
+				 ->set_description( __( 'No image should be without alt-attribute, so if there are some without one, an empty one will be added.', 'straightvisions_100' ) )
+				 ->load_type( 'checkbox' );
+		
+		$this->s['type_attr'] =
+			$this->get_setting()
+				 ->set_ID( 'type_attr' )
+				 ->set_title( __( 'Remove type-attributes from style and script tags', 'straightvisions_100' ) )
+				 ->set_description( __( 'These are not needed for standard purposes anymore, W3C recommends to remove them if not needed. You will reduce your pageload as well.', 'straightvisions_100' ) )
+				 ->load_type( 'checkbox' );
+		
+		return $this;
+	}
 	public function wp_start(){
 		ob_start();
 	}
-
 	public function wp_end(){
 		$output				= ob_get_contents();
-		$output				= $this->remove_type_attr($this->add_alt_tags($output));
+		
+		if($this->s['alt_attr']->run_type()->get_data()) {
+			$output			= $this->add_alt_tags( $output );
+		}
+		
+		if($this->s['type_attr']->run_type()->get_data()) {
+			$output			= $this->remove_type_attr($output);
+		}
+		
 		ob_end_clean();
 		echo $output;
 		ob_start();
 	}
-
 	public function remove_type_attr($input){
 		$input = str_replace("type='text/javascript'", '', $input);
 		$input = str_replace('type="text/javascript"', '', $input);
@@ -58,7 +142,6 @@ class sv_cleanup extends init {
 
 		return $input;
 	}
-
 	public function add_alt_tags($content){
 		preg_match_all('/<img (.*?)\/>/', $content, $images);
 		if(!is_null($images))
@@ -74,7 +157,6 @@ class sv_cleanup extends init {
 		}
 		return $content;
 	}
-
 	public function wp_print_styles(){
 		if(wp_style_is('wp-mediaelement')){
 			wp_dequeue_style('wp-mediaelement');
