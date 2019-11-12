cde41c766d4c88e46f99faf691a7a151ff427fc1
Matthias Bathke <info@straightvisions.com>
2019-11-12 21:05:12 +0100

    REFACTOR SETTINGS

--- lib/backend/tpl/settings.php
+++ lib/backend/tpl/settings.php
@@ -6,25 +6,25 @@
 		<h3 class="divider"><?php _e( 'Decrease PageLoad', 'sv100_companion' ); ?></h3>
 		<div class="sv_setting_flex">
 			<?php
-				echo $module->get_settings()['jquery_migrate']->run_type()->form();
-				echo $module->get_settings()['meta_data']->run_type()->form();
-				echo $module->get_settings()['emoji_styles']->run_type()->form();
+				echo $module->get_setting('jquery_migrate')->run_type()->form();
+				echo $module->get_setting('meta_data')->run_type()->form();
+				echo $module->get_setting('emoji_styles')->run_type()->form();
 			?>
 		</div>
 		
 		<h3 class="divider"><?php _e( 'Improve Scriptloading', 'sv100_companion' ); ?></h3>
 		<div class="sv_setting_flex">
 			<?php
-				echo $module->get_settings()['wp_media']->run_type()->form();
-			echo $module->get_settings()['css_lazyload']->run_type()->form();
+				echo $module->get_setting('wp_media')->run_type()->form();
+			echo $module->get_setting('css_lazyload')->run_type()->form();
 			?>
 		</div>
 		
 		<h3 class="divider"><?php _e( 'Optimize W3C Validation', 'sv100_companion' ); ?></h3>
 		<div class="sv_setting_flex">
 			<?php
-				echo $module->get_settings()['alt_attr']->run_type()->form();
-				echo $module->get_settings()['type_attr']->run_type()->form();
+				echo $module->get_setting('alt_attr')->run_type()->form();
+				echo $module->get_setting('type_attr')->run_type()->form();
 			?>
 		</div>
 		<?php
