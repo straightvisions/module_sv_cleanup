25ea98e5db593a25f7a4ae7e7a7a5aefac1801bc
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-12-07 17:46:46 +0100

    ADD COMPATIBILITY: CSS LAZYLOADING WP ROCKET

--- sv_cleanup.php
+++ sv_cleanup.php
@@ -44,7 +44,7 @@ class sv_cleanup extends modules {
 		}
 
 		if($this->get_setting('css_lazyload')->get_data()){
-			add_action('init', array($this, 'wp_init'));
+			add_action('init', array($this, 'wp_init'), 999999);
 		}
 
 		if($this->get_setting('prevent_fouc')->get_data() && !is_admin()){
@@ -82,9 +82,62 @@ class sv_cleanup extends modules {
 		add_action('wp_head', array($this, 'wp_start'), 1);
 		add_action('wp_footer', array($this, 'wp_end'), 9999999);
 	}
+	public function load_settings(): sv_cleanup {
+		$this->get_setting('jquery_migrate')
+			->set_title( __( 'Disable jQuery Migrate', 'sv100_companion' ) )
+			->set_description( __( 'In most cases, you will not need jQuery-Migrate. Disabling this script will reduce pageload and improve Pagespeed. Check for Javascript-Errors in Frontend after activating this.', 'sv100_companion' ) )
+			->load_type( 'checkbox' );
+
+		$this->get_setting('meta_data')
+			->set_title( __( 'Remove non-critical meta data', 'sv100_companion' ) )
+			->set_description( __( 'Removes some lines of HTML-Meta-Data which are not critical for your site, but saves some byte of code in the frontend: rest_output_link_wp_head, wp_oembed_add_discovery_links, rsd_link, wlwmanifest_link, wp_shortlink_wp_head, wp_generator', 'sv100_companion' ) )
+			->load_type( 'checkbox' );
+
+		$this->get_setting('emoji_styles')
+			->set_title( __( 'Removes Emoji Styles', 'sv100_companion' ) )
+			->set_description( __( 'Instead of loading those styles from WP, default browser emojis will be displayed', 'sv100_companion' ) )
+			->load_type( 'checkbox' );
+
+		$this->get_setting('wp_media')
+			->set_title( __( 'Load WP Media Styles inline', 'sv100_companion' ) )
+			->set_description( __( 'To optimize your Pagespeed Score, you may need to load WP Media Styles inline if loaded. Activate this, if Pagespeed Test Tool says external WP Media Styles are renderblocking.', 'sv100_companion' ) )
+			->load_type( 'checkbox' );
+
+		$this->get_setting('css_lazyload')
+			->set_title( __( 'Lazyload attached CSS files', 'sv100_companion' ) )
+			->set_description( sprintf(__( 'Attached CSS should be lazyloaded. Even with WP-Rocket this is not solved completely %1$s(see issue)%2$s', 'sv100_companion' ),
+				'<a target="_blank" href="' . esc_url( 'https://github.com/wp-media/wp-rocket/issues/1814' ) . '">',
+				'</a>'
+			) )
+			->load_type( 'checkbox' );
+
+		$this->get_setting('prevent_fouc')
+			->set_title( __( 'Prevent FOUC', 'sv100_companion' ) )
+			->set_description( __( 'Content will be hidden until DOM is ready to prevent flash of unstyled content (FOUC). If there is any javascript-error on your site, this could prevent content to be shown to your visitors. Using this Anti-FOUC-Feature should be avoided by reducing pagesize and optimizing CSS delivery.', 'sv100_companion' ) )
+			->load_type( 'checkbox' );
+
+		$this->get_setting('alt_attr')
+			->set_title( __( 'Add alt-attributes to images if missing', 'sv100_companion' ) )
+			->set_description( __( 'No image should be without alt-attribute, so if there are some without one, an empty one will be added.', 'sv100_companion' ) )
+			->load_type( 'checkbox' );
+
+		$this->get_setting('type_attr')
+			->set_title( __( 'Remove type-attributes from style and script tags', 'sv100_companion' ) )
+			->set_description( __( 'These are not needed for standard purposes anymore, W3C recommends to remove them if not needed. You will reduce your pageload as well.', 'sv100_companion' ) )
+			->load_type( 'checkbox' );
+
+		$this->get_setting('wp_embed')
+			->set_title( __( 'Disable WP Embed', 'sv100_companion' ) )
+			->set_description( __( 'For PageSpeed or GDPR Purposes you may want to disable WP Embed feature.', 'sv100_companion' ) )
+			->load_type( 'checkbox' );
+
+		return $this;
+	}
 	public function wp_init(){
 		if(!defined('WP_ROCKET_PATH')) {
 			add_filter('style_loader_tag', array($this, 'css_lazyload'));
+		}else{
+			add_filter('rocket_buffer', array($this, 'css_lazyload'), 999999);
 		}
 	}
 	public function css_lazyload($buffer){
@@ -116,63 +169,12 @@ class sv_cleanup extends modules {
 		remove_action('wp_head', 'wp_shortlink_wp_head');
 		remove_action('wp_head', 'wp_generator');
 	}
-	public function load_settings(): sv_cleanup {
-			$this->get_setting('jquery_migrate')
-				 ->set_title( __( 'Disable jQuery Migrate', 'sv100_companion' ) )
-				 ->set_description( __( 'In most cases, you will not need jQuery-Migrate. Disabling this script will reduce pageload and improve Pagespeed. Check for Javascript-Errors in Frontend after activating this.', 'sv100_companion' ) )
-				 ->load_type( 'checkbox' );
-			
-			$this->get_setting('meta_data')
-				 ->set_title( __( 'Remove non-critical meta data', 'sv100_companion' ) )
-				 ->set_description( __( 'Removes some lines of HTML-Meta-Data which are not critical for your site, but saves some byte of code in the frontend: rest_output_link_wp_head, wp_oembed_add_discovery_links, rsd_link, wlwmanifest_link, wp_shortlink_wp_head, wp_generator', 'sv100_companion' ) )
-				 ->load_type( 'checkbox' );
-			
-			$this->get_setting('emoji_styles')
-				 ->set_title( __( 'Removes Emoji Styles', 'sv100_companion' ) )
-				 ->set_description( __( 'Instead of loading those styles from WP, default browser emojis will be displayed', 'sv100_companion' ) )
-				 ->load_type( 'checkbox' );
-			
-			$this->get_setting('wp_media')
-				 ->set_title( __( 'Load WP Media Styles inline', 'sv100_companion' ) )
-				 ->set_description( __( 'To optimize your Pagespeed Score, you may need to load WP Media Styles inline if loaded. Activate this, if Pagespeed Test Tool says external WP Media Styles are renderblocking.', 'sv100_companion' ) )
-				 ->load_type( 'checkbox' );
-			
-			$this->get_setting('css_lazyload')
-				 ->set_title( __( 'Lazyload attached CSS files', 'sv100_companion' ) )
-				 ->set_description( sprintf(__( 'Attached CSS should be lazyloaded. Even with WP-Rocket this is not solved completely %1$s(see issue)%2$s', 'sv100_companion' ),
-					 '<a target="_blank" href="' . esc_url( 'https://github.com/wp-media/wp-rocket/issues/1814' ) . '">',
-					 '</a>'
-				 ) )
-				 ->load_type( 'checkbox' );
-
-			$this->get_setting('prevent_fouc')
-				->set_title( __( 'Prevent FOUC', 'sv100_companion' ) )
-				->set_description( __( 'Content will be hidden until DOM is ready to prevent flash of unstyled content (FOUC). If there is any javascript-error on your site, this could prevent content to be shown to your visitors. Using this Anti-FOUC-Feature should be avoided by reducing pagesize and optimizing CSS delivery.', 'sv100_companion' ) )
-				->load_type( 'checkbox' );
-
-			$this->get_setting('alt_attr')
-				 ->set_title( __( 'Add alt-attributes to images if missing', 'sv100_companion' ) )
-				 ->set_description( __( 'No image should be without alt-attribute, so if there are some without one, an empty one will be added.', 'sv100_companion' ) )
-				 ->load_type( 'checkbox' );
-			
-			$this->get_setting('type_attr')
-				 ->set_title( __( 'Remove type-attributes from style and script tags', 'sv100_companion' ) )
-				 ->set_description( __( 'These are not needed for standard purposes anymore, W3C recommends to remove them if not needed. You will reduce your pageload as well.', 'sv100_companion' ) )
-				 ->load_type( 'checkbox' );
-
-		$this->get_setting('wp_embed')
-			->set_title( __( 'Disable WP Embed', 'sv100_companion' ) )
-			->set_description( __( 'For PageSpeed or GDPR Purposes you may want to disable WP Embed feature.', 'sv100_companion' ) )
-			->load_type( 'checkbox' );
-		
-		return $this;
-	}
 	public function wp_start(){
 		ob_start();
 	}
 	public function wp_end(){
 		$output				= ob_get_contents();
-		
+		ob_end_clean();
 		if($this->get_setting('alt_attr')->get_data()) {
 			$output			= $this->add_alt_tags( $output );
 		}
